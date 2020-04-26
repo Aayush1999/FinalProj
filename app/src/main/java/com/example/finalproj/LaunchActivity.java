260edb4f456f9dc941b84d3ad3256c624bb1c50d
Aayush Aggarwal <aayush3@illinois.edu>
2020-04-26 10:48:48 +0400

    mediFind v1

--- app/src/main/java/com/example/finalproj/LaunchActivity.java
+++ app/src/main/java/com/example/finalproj/LaunchActivity.java
@@ -2,7 +2,9 @@ package com.example.finalproj;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class LaunchActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class LaunchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_launch);
+        Button action = findViewById(R.id.FeverButton);
+        Intent intent = new Intent(this, FeverMed.class);
+        action.setOnClickListener(v -> {
+            startActivity(intent);
+            finish();
+        });
+        Button action2 = findViewById(R.id.CoughButton);
+        Intent intent2 = new Intent(this, CoughMeds.class);
+        action2.setOnClickListener(v -> {
+            startActivity(intent2);
+            finish();
+        });
+
     }
 }
